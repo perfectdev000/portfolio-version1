ac2ad3b7ab77fc6369d8b7e62a7206b3f811347b
Zeus <84690417+darkrut22@users.noreply.github.com>
2022-01-05 00:09:20 +0900

    add mail feature

--- pages/glory/index.tsx
+++ pages/glory/index.tsx
@@ -2,15 +2,8 @@ import React from 'react';
 import Head from 'next/head'
 import Script from 'next/script'
 
-import emailjs from 'emailjs-com'
-import emailKey from './emailkey'
-
 import { FaAngleUp, } from 'react-icons/fa';
 
-import Slider from 'react-slick';
-
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 import Header from "components/header/Header"
 import Footer from 'components/footer/Footer';
 
